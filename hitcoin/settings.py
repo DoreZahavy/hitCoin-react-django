7a50a6677f559b36123612e2a123a079a44c8377
Dore Zahavy <dorezahavy@gmail.com>
2023-09-08 10:05:25 +0300

    fix allowed host bug and reorganize public dir

--- hitcoin/settings.py
+++ hitcoin/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'public/assets')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'public/static')]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifesrStaticFilesStorage'
 
